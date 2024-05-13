e192ddf094f827a2e47e9cdcd81a614b6c25946a
David R Newman <drn@ecs.soton.ac.uk>
2024-05-13 18:11:10 +0100

    Adds jquery as ingredient dependency

--- cfg.d/z_richtext.pl
+++ cfg.d/z_richtext.pl
@@ -1,3 +1,5 @@
+$c->{deps}->{"ingredients/richtext"} = [ "ingredients/jquery" ];
+
 #explicitly specify xapian indexing method
 $c->{xapian}->{indexing_methods} = {} unless defined $c->{xapian}->{indexing_methods};
 $c->{xapian}->{indexing_methods}->{'EPrints::MetaField::Richtext'} = 'text';
