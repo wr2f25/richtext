d8ab3411b7b0ec67925b13c9c6d2a268edffa30f
David R Newman <drn@ecs.soton.ac.uk>
2025-01-13 15:02:01 +0000

    Adds hashref for HTML entities

--- cfg.d/z_richtext.pl
+++ cfg.d/z_richtext.pl
@@ -6,3 +6,12 @@ if ( EPrints->human_version =~ m/^3.4/ )
 #explicitly specify xapian indexing method
 $c->{xapian}->{indexing_methods} = {} unless defined $c->{xapian}->{indexing_methods};
 $c->{xapian}->{indexing_methods}->{'EPrints::MetaField::Richtext'} = 'text';
+
+# Define HTML entities to support HTML decoding of rich text strings
+# E.g. $value =~ s/([&<>"])/$c->{html_entities}->{$1}/g;
+$c->{html_entities} = {
+    '&' => '&amp;',
+    '>' => '&gt;',
+    '<' => '&lt;',
+    '"' => '&quot;',
+};
